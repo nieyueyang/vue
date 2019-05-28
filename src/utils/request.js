42f210227cfa7d4d4db920ec1c64ff4f85c9177f
nieyueyang <nieyueyang@163.com>
2019-05-28 20:19:22 +0800

    vue新增

---
+++ src/utils/request.js
@@ -0,0 +1,119 @@
+import axios from "axios";
+import qs from 'querystring';
+import Vue from 'vue';
+let vue = new Vue();
+
+let prefixUrl;
+prefixUrl = process.env.NODE_ENV === 'development' ? '/api' : window.server+'/sso';// 'http://127.0.0.1:8080/sso';
+
+function createLicenseDoc(){
+  var img = "<div style='text-align: center;margin-top: 80px'><img src='../assets/images/error_02.png'/></div>"
+  document.body.innerHTML = img
+}
+
+axios.defaults.timeout = 30*60*1000;  // 30分钟
+
+export default (url, options) => {
+  debugger
+  let param = options.param||{}
+  param['_R']=Math.random();
+
+  return new Promise((resolve) => {
+    let data = {}
+    if (options.formatJSon) {
+      data = options.data
+    } else {
+      data = qs.stringify(options.data)
+    }
+
+    axios({
+      method: options.method,
+      url: prefixUrl + url,
+      data: data,
+      params: param,
+      headers: options.formatJSon ? options.headers : {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'X-Requested-With': 'XMLHttpRequest'
+      }
+    })
+      .then((resp) => {
+          if(resp.data.code == 200){
+                debugger
+                resolve(resp.data);
+              }else if(resp.data.code ==400){
+                //特殊状态进行处理
+                resolve(resp.data);
+              }else{
+                //给出提示
+          }
+
+      })
+      .catch((param) => {
+        // if (param.response.status == 306) {
+        //   window.parent.location.href = '/wbalone/pages/login/login.html?r=L3BvcnRhbC8'
+        // }
+        // Message.create({ content: '数据返回出错：1、请确保服务运行正常；2、请确保您的前端工程代理服务正常；3、请确认您已在本地登录过应用平台', color : 'danger'});
+       // reject(param)
+      })
+  })
+}
+
+
+
+
+
+export const axiosPost = (url, options) => {
+    return new Promise((resolve) => {
+          axios({
+              method:"post",
+              url: prefixUrl + url,
+              data:qs.stringify({
+                ...options.data
+              }),
+              headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded'
+              }
+              
+          }).then( (response)=> {
+                responseHandle(response.data,resolve);
+            }).catch(function (error){
+              console.log(error);
+            });
+    })
+}
+
+
+export const axiosGet = (url, options) => {
+  let param = options.param||{}
+  param['_R']=Math.random();
+  let data = {}
+  data = qs.stringify(options.data)
+
+  return new Promise((resolve) => {
+        axios({
+            method:"get",
+            url: prefixUrl + url,
+            data: data,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).then( (response)=> {
+              responseHandle(response.data,resolve);
+          }).catch(function (error){
+            console.log(error);
+          });
+  })
+}
+
+//返回数据处理
+function responseHandle(data,resolve){
+  if(data.code == 200){  //访问成功
+    resolve(data.data);
+  }else if (data.code == 20003 || data.code == 20001){ //需要特殊处理的数据
+      vue.$message({type: 'warning', message:  data.msg})
+
+  }else{  //弹出msg提示
+      vue.$message({type: 'warning', message:  data.msg})
+     
+  }
+}
---
+++ src/views/Login.vue
@@ -0,0 +1,96 @@
+<template>
+  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">
+    <h3 class="title">系统登录</h3>
+    <el-form-item prop="account">
+      <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
+    </el-form-item>
+    <el-form-item prop="password">
+      <el-input type="password" v-model="ruleForm2.password" auto-complete="off" placeholder="密码"></el-input>
+    </el-form-item>
+    <el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox>
+    <el-form-item style="width:100%;">
+      <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">登录</el-button>
+      <!--<el-button @click.native.prevent="handleReset2">重置</el-button>-->
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+  //import { requestLogin } from '../api/api';
+  //import request from '../utils/request.js';
+  import { axiosPost } from '../utils/request.js';
+  //import qs from 'querystring'
+
+
+  export default {
+    data() {
+      return {
+        logining: false,
+        ruleForm2: {
+          account: '',
+          checkPass: ''
+        },
+        rules2: {
+          account: [
+            { required: true, message: '请输入账号', trigger: 'blur' },
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+          ]
+        },
+        checked: true
+      };
+    },
+    methods: {
+      handleReset2() {
+        this.$refs.ruleForm2.resetFields();
+      },
+      handleSubmit2(ev) {
+        debugger
+        var _this = this;
+        this.$refs.ruleForm2.validate((valid) => {
+          debugger
+          if (valid) {
+              axiosPost('/login', {
+                  method: "POST",
+                  data: {"account": this.ruleForm2.account,"password": this.ruleForm2.password}
+              }).then((data) => {
+                  debugger
+                  sessionStorage.setItem('user', JSON.stringify(this.ruleForm2.account));
+                  this.$router.push({ path: '/table' });
+              }).catch()
+            
+          } else {
+            console.log('不能提交!!');
+            return false;
+          }
+        });
+      }
+    }
+  }
+
+</script>
+
+<style lang="scss" scoped>
+  .login-container {
+    /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+    background-clip: padding-box;
+    margin: 180px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+    .title {
+      margin: 0px auto 40px auto;
+      text-align: center;
+      color: #505458;
+    }
+    .remember {
+      margin: 0px 0px 35px 0px;
+    }
+  }
+</style>
